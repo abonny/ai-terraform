70f0afa020c5c851e59b1eca4dd3ff70721cd6b6
Luke Pahler <38334809+abonny@users.noreply.github.com>
2025-07-16 16:37:20 -0600

    Globally unique Storage Account

--- azure-function-app-caller/function-app-example.tf
+++ azure-function-app-caller/function-app-example.tf
@@ -2,5 +2,5 @@ module "function_app" {
   source              = "../azure-function-app"
   resource_group_name = "rg-ai-terraform"
   location            = "West US"
-  function_app_name   = "myfuncapp"
+  function_app_name   = "my3cloudfuncapp"
 }
