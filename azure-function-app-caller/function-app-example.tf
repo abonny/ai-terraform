d4f118229c094692cce11b75d955d5088b414ee3
Luke Pahler <38334809+abonny@users.noreply.github.com>
2025-07-16 14:53:13 -0600

    Make this work

--- azure-function-app-caller/function-app-example.tf
+++ azure-function-app-caller/function-app-example.tf
@@ -1,6 +1,6 @@
 module "function_app" {
   source              = "../azure-function-app"
-  resource_group_name = "my-rg"
-  location            = "East US"
-  function_app_name   = "my-func-app"
+  resource_group_name = "rg-ai-terraform"
+  location            = "West US"
+  function_app_name   = "myfuncapp"
 }
